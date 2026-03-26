9a39dcc345afe5322fa11c6b8d802ef523d00648
PriyankaSankaranarayanan <s.priyankameenakshi2208@gmail.com>
2026-03-26 12:47:32 +0530

    added maven project

--- Hello-App/src/main/java/Helloapp.java
+++ Hello-App/src/main/java/Helloapp.java
@@ -1,19 +1,5 @@
 public class Helloapp {
     public static void main(String[] args) {
-        if (args.length == 0) {
-            System.out.println("Hello, World!");
-        } else {
-            System.out.print("Hello ");
-
-            int count = 0;
-            for (String name : args) {
-                System.out.print(name);
-                count++;
-                if (count < args.length) {
-                    System.out.print(", ");
-                }
-            }
-            System.out.println();
+            System.out.println("Hello");
         }
     }
-}
