d0025b1a69dc634853fdb6fdf35426a702317c79
PriyankaSankaranarayanan <s.priyankameenakshi2208@gmail.com>
2026-03-26 13:54:20 +0530

    Added UC6 Assignment

---
+++ Hello-App/src/main/java/HelloApp_UC6.java
@@ -0,0 +1,22 @@
+public class HelloApp_UC6 {
+    public static void main(String[] args) {
+
+        // Default message
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+            return;
+        }
+
+        String result = "";
+
+        // Enhanced for loop
+        for (String name : args) {
+            result += name + ", ";
+        }
+
+        // Remove last comma and space
+        result = result.substring(0, result.length() - 2);
+
+        System.out.println("Hello, " + result + "!");
+    }
+}
